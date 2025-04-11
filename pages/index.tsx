11348651c21820facd9354521a7b1ae94f06a6f9
daiki568 <xdaiki568@gmail.com>
2025-04-11 19:23:29 +0900

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,8 @@
 export default function Home() {
-  return <h1>ようこそ！昼食予約アプリへ</h1>;
+  return (
+    <div style={{ minHeight: '100vh', overflowY: 'auto', padding: '24px' }}>
+      <h1>ようこそ！昼食予約アプリへ</h1>
+      {/* 必要なら今後他の要素もここに追加可能 */}
+    </div>
+  );
 }
