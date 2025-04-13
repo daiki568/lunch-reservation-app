d5b48b28dde3ec13114326813d67df63e4db48a8
daiki568 <xdaiki568@gmail.com>
2025-04-14 02:53:00 +0900

    Delete pages directory

--- pages/index.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ minHeight: '100vh', overflowY: 'auto', padding: '24px' }}>
-      <h1>ようこそ！昼食予約アプリへ</h1>
-      {/* 必要なら今後他の要素もここに追加可能 */}
-    </div>
-  );
-}
